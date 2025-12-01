Fix bugs in localization computation & eval script

cub/dataset.py
@@ -100,7 +100,7 @@ class CUBLocalizationDataset(Dataset):
 
         # Load pickled data
         self.data = []
-        assert "test" in pkl_path or "validation" in pkl_path
+        assert "test" in pkl_path or "val" in pkl_path
         self.data = pickle.load(open(pkl_path, 'rb'))
 
         # Construct paths
@@ -225,12 +225,12 @@ class CUBLocalizationDataset(Dataset):
         for info in part_infos:
             #info: part_id, x1, y1, visible
             if info[-1] == 0.0: #part not visible, no gt
-                part_masks.append([0, 0, 0, 0]) # Must fill with dummy values for batching
+                part_masks.append(torch.zeros(4, dtype=torch.int16)) # Must fill with dummy values for batching
                 continue
             gt = (info[1], info[2])
             #change from x, y, w, h to x1, y1, x2, y2
             transformed_bird_BB = [bird_bb[0], bird_bb[1], bird_bb[0] + bird_bb[2], bird_bb[1] + bird_bb[3]]
-            part_masks.append(self.get_KP_BB(gt, mask_h, mask_w, transformed_bird_BB))
+            part_masks.append(torch.tensor(self.get_KP_BB(gt, mask_h, mask_w, transformed_bird_BB), dtype=torch.int16))
 
         return part_masks
 
@@ -304,106 +304,6 @@ class CUBLocalizationDataset(Dataset):
                 bb_info = [float(x) for x in bb_info]
                 
                 self.imgID_to_birdBB[int(id_str)] = bb_info
-
-
-class CUBDatasetPartSegmentations(Dataset):
-    """
-    Returns a compatible Torch Dataset object customized for the CUB dataset
-    """
-
-    def __init__(self, pkl_path, data_dir, resol, transform):
-        """
-        Args:
-            pkl_path: Full path to test or validation pkl
-            data_dir: Full path to the data directory of the CUB dataset, should point to the root.
-            transform: whether to apply any special transformation. Default = None, i.e. use standard ImageNet preprocessing
-        """
-        self.data = []
-        assert "test" in pkl_path or "validation" in pkl_path
-        self.data = pickle.load(open(pkl_path, 'rb'))
-
-        # Filter data to remove all classes for which no part segmentations exist (--> class_label > 70), +1 as it is zero-indexed
-        self.data = [d for d in self.data if d.get("class_label") + 1 <= 70]
-
-        self.transform = transform
-        self.resol = resol
-        self.data_dir = data_dir
-        self.image_dir = os.path.join(data_dir, "images")
-        self.part_seg_dir = os.path.join(data_dir, "part_segmentations")
-
-        self.mask_transform = transforms.Compose([
-            transforms.CenterCrop(299),         
-            transforms.ToTensor(),                          
-            lambda t: (t > 0.5).float()  # binarize
-        ])
-
-    def __len__(self):
-        return len(self.data)
-
-    def _load_mask(self, path_to_mask):
-        # Not all parts are segmented for each img (e.g. occlusion), fill with zeros
-        if not os.path.exists(path_to_mask):
-            return torch.zeros(1, self.resol, self.resol, dtype=torch.float32)
-
-        # If it exists: get part segmentation mask for this image / part pair
-        mask = Image.open(path_to_mask).convert("L")  # 'L' = grayscale
-
-        # Apply transformations to it (center crop like image, then binarize and add dummy channel dim)
-        return self.mask_transform(mask)
-    
-    def __getitem__(self, idx):
-        img_data = self.data[idx]
-        img_path = img_data['img_path']
-        
-        # Trim unnecessary paths, load image
-        path_parts = img_path.split('/')
-        cub_index = path_parts.index("images")
-        img_source_path = os.path.join(self.image_dir , "/".join(path_parts[cub_index+1:]))
-        img = Image.open(img_source_path).convert('RGB')
-
-        if self.transform:
-            img = self.transform(img)
-
-        # Get class and attribute labels
-        class_label = img_data['class_label']
-        attr_label = img_data['attribute_label']
-
-        # Get localization data, i.e. part segmentation masks and bounding box data
-        part_seg_masks = self._get_part_seg_masks(img_path)
-        
-        return img, class_label, attr_label, part_seg_masks
-
-    def _get_part_seg_masks(self, img_path):
-
-        # Get the class number, e.g. 002 for Laysan_Albatross, strip initial numbers, also get img name w/out extension
-        class_nr = str(int(img_path.split("/")[-2][:3]))
-        img_name = img_path.split("/")[-1].split(".")[0]
-
-        # Collect all part segmentation masks
-        part_lst = []
-        for part in PART_SEG_GROUPS:
-
-            # IMPORTANT: For left / right distinctions, we add the masks to one.
-            part_list = [part]
-            if part in ["eye", "wing", "leg"]:
-                part_list = [f"right_{part}", f"left_{part}"]
-
-            tmp_masks = []
-            for tmp_part in part_list:
-                tmp_masks.append(
-                    self._load_mask(
-                        os.path.join(self.part_seg_dir, "AnnotationMasksPerclass", class_nr, f"{img_name}_{tmp_part}.png")
-                    )
-                )
-            
-            # Combine masks, if needed, and add to part_lst.
-            if len(tmp_masks) == 1:
-                combined_mask = tmp_masks[0]
-            else:
-                combined_mask = ((tmp_masks[0] > 0) | (tmp_masks[1] > 0)).to(tmp_masks[0].dtype)
-            part_lst.append(combined_mask)
-
-        return torch.cat(part_lst, dim=0)  # (num_parts, H, W)
 
 
 # TODO: load pkl from path + corresponding file name

eval.py
@@ -3,10 +3,10 @@ Evaluate trained models on the official CUB test set
 """
 
 import os
-import sys
 import torch
 import argparse
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 import yaml
 
 from cub.dataset import CUBLocalizationDataset
@@ -17,8 +17,9 @@ from localization.localization_accuracy import calculate_average_partwise_locali
 from models.apn_baseline import load_apn_baseline
 from saliency.saliency import get_saliency_map_and_scores_and_prediction
 from utils.mappings import MAP_CUB_PARTS_GROUPS_TO_CUB_ATTRIBUTE_IDS, MAP_PART_SEG_GROUPS_TO_CUB_GROUPS
+from utils.index_translation import map_attribute_ids_from_cub_to_cbm
 from utils.eval_utils import get_eval_transform_for_model
-from utils.train_utils import prepare_model, model_by_mode
+from utils.train_utils import accuracy, prepare_model, model_by_mode
 
 
 def create_model(args):
@@ -38,13 +39,11 @@ def eval(args):
         TODO
     """
 
-    print("ALLO")
     # Create the model and load weights
     model = create_model(args)
     model = torch.load(os.path.join(args.log_dir, "best_model_1.pth"))
     model, device = prepare_model(model)
     model.eval()
-    print(model)
 
     transform, transform_mean, transform_std, img_size = get_eval_transform_for_model(model, args)
     # TODO: APN uses 312 attributes, ours only 112, so adjust the code to work for both
@@ -52,7 +51,6 @@ def eval(args):
     # Data management
     pkl_path = os.path.join(BASE_DIR, args.split_dir)
     data_dir = os.path.join(BASE_DIR, args.data_dir)
-    print(pkl_path)
     dataset = CUBLocalizationDataset(pkl_path, data_dir, img_size, transform)
     loader = DataLoader(
         dataset, 
@@ -65,18 +63,21 @@ def eval(args):
     )
 
     # Creates a mapping tensor of attribute ids to their respective part segmentation group, removes unmatched entries, returns kept attribute names
-    map_attr_id_to_part_seg_group, attribute_names = create_mapping_attributes_to_part_seg_group(args.image_dir, device)
+    map_attr_id_to_part_seg_group, attribute_names, unmatched_attr_mask = create_mapping_attributes_to_part_seg_group(args.data_dir, device)
 
     # Create a mapping tensor for the localization accuracy as well
     loc_acc_collector = []
-    map_part_to_attr_loc_acc = create_part_attribute_mapping_tensor(MAP_CUB_PARTS_GROUPS_TO_CUB_ATTRIBUTE_IDS)
+    map_parts_to_attributes = map_attribute_ids_from_cub_to_cbm(MAP_CUB_PARTS_GROUPS_TO_CUB_ATTRIBUTE_IDS)
+    map_part_to_attr_loc_acc = create_part_attribute_mapping_tensor(map_parts_to_attributes, device)
 
-    # Collecting IoU values across batches for proper mean
+    # Collecting IoU and accuracy values across batches for proper mean
     iou_sum_per_attr = torch.zeros(len(attribute_names), device=device)
     iou_count_per_attr = torch.zeros(len(attribute_names), device=device)
+    acc_sum = 0
+    acc_count = 0
 
     with torch.no_grad():
-        for data_idx, data in enumerate(loader):
+        for data_idx, data in enumerate(tqdm(loader, desc="Evaluating batches")):
 
             # Cast data to device
             data = [v.to(device) if torch.is_tensor(v) else v for v in data]
@@ -84,10 +85,14 @@ def eval(args):
             inputs, labels, attr_labels, part_seg_masks, part_bbs = data
             attr_labels = torch.stack(attr_labels).t()  # N x A
 
-            # Pass through model, get model prediction and saliency map
+            # Pass through model, get model prediction and saliency map per attribute
             output = model(inputs.to(device))
             pred, scores, saliency_maps = get_saliency_map_and_scores_and_prediction(output, args)
 
+            # Compute classification accuracy
+            acc_sum += accuracy(pred, labels, topk=(1,))[0]
+            acc_count += 1
+            
             # Compute localization accuracy and collect into our collector
             compute_localization_accuracy(
                 scores,
@@ -100,7 +105,10 @@ def eval(args):
             )
 
             # Compute IoU between part segmentation masks and our saliency maps, for each attribute
-            spr, cpr, saliency_maps_upsampled, seg_masks_per_attribute = compute_IoU_to_seg_masks(saliency_maps, part_seg_masks)
+            # Map out unmapped attributes from the saliency mask
+            spr, cpr, saliency_maps_upsampled, seg_masks_per_attribute = compute_IoU_to_seg_masks(
+                saliency_maps[:, unmatched_attr_mask], part_seg_masks, map_attr_id_to_part_seg_group
+            )
             iou_sum_per_attr += spr
             iou_count_per_attr += cpr
 
@@ -115,7 +123,12 @@ def eval(args):
     compute_mIoU_statistics(iou_sum_per_attr, iou_count_per_attr, attribute_names, map_attr_id_to_part_seg_group)
 
     # Compute part localization statistics, considering our grouping of attributes to groups.
-    calculate_average_partwise_localization_accuracy(loc_acc_collector, MAP_PART_SEG_GROUPS_TO_CUB_GROUPS, IoU_thr=args["IoU_threshold"])
+    calculate_average_partwise_localization_accuracy(loc_acc_collector, MAP_PART_SEG_GROUPS_TO_CUB_GROUPS, IoU_thr=args.IoU_threshold)
+
+    # Compute mean classification accuracy and print
+    mean_acc = acc_sum / (acc_count + 1e-7)
+    print("\n--------- CLASSIFICATION ACCURACY ---------\n")
+    print(f"Mean Classification Accuracy: {mean_acc.item():.4f}")
 
 
 if __name__ == '__main__':
@@ -142,10 +155,6 @@ if __name__ == '__main__':
     out_folder_path = os.path.join(args.log_dir, "part_seg_vis")
     os.makedirs(out_folder_path, exist_ok=True)
     args.out_dir_part_seg = out_folder_path
-
-    # Create .txt for output of this script, write everything to there
-    log_file = os.path.join(args.log_dir, "eval.txt")
-    sys.stdout = open(log_file, "w")
 
     # Run main evaluation
     eval(args)

localization/localization_accuracy.py
@@ -40,7 +40,6 @@ def compute_localization_accuracy(
 
     #get argmax attribute per part
     argmax_per_part = [] #max index per part, -1 if part not present
-    
     for part in part_dict.values():
         #take argmax of each part group
         subset = pre_attri[:, part_attribute_mapping_tensor[part]]
@@ -76,7 +75,7 @@ def compute_localization_accuracy(
     resized_heatmaps = torch.nn.functional.interpolate(heatmaps, size=img_size, mode='bilinear', align_corners=False)
 
     # Compute sliding window from heatmaps
-    optimal_masks_batch = compute_optimal_masks_per_mask(torch.from_numpy(resized_heatmaps), bounding_box_per_part.to(heatmaps.device))
+    optimal_masks_batch = compute_optimal_masks_per_mask(resized_heatmaps, bounding_box_per_part)
     optimal_masks_batch[~valid_mask] = 0
 
     # Get the IoU between the bounding boxes and our heatmaps, per part over all images, set to -1 for non-existing parts
@@ -90,10 +89,10 @@ def compute_localization_accuracy(
         collector_list.append(sub_res)
 
 
-def create_part_attribute_mapping_tensor(part_attribute_mapping: Dict[str, List[int]]):
+def create_part_attribute_mapping_tensor(part_attribute_mapping: Dict[str, List[int]], device):
     part_attribute_mapping_tensor = {}
     for part, attr_list in part_attribute_mapping.items():
-        part_attribute_mapping_tensor[part] = torch.tensor(attr_list).to("cuda" if torch.cuda.is_available() else "cpu")
+        part_attribute_mapping_tensor[part] = torch.tensor(attr_list).to(device)
     return part_attribute_mapping_tensor
 
 
@@ -187,6 +186,12 @@ def calculate_average_partwise_localization_accuracy(all_ious:list[dict], subgro
 
     mean_iou = [x for x in res.values() if x != -1]
     mean_iou_acc = sum(mean_iou)/len(mean_iou)
+
+    print("\n--------- LOCALIZATION ACCURACY ---------\n")
+
+    for group_name, acc in res.items():
+        print(f"Group {group_name} - LocAcc: {acc:.4f}")
+    print(f"\nMean LocAcc: {mean_iou_acc:.4f}")
 
     return res, mean_iou_acc
 

train_protocbm.py
@@ -119,7 +119,7 @@ def epoch_wrapper(
 
 
 def train(model: nn.Module, args: Namespace) -> float:
-    model = prepare_model(model)
+    model, _ = prepare_model(model)
 
     logger, tb_writer = logger_and_summarywriter(args)
 