4e2f686ec703682ea1bc0c9c2bbd03dc9b3a5167
Melih <melih.oezkan@stud.tu-darmstadt.de>
2025-12-03 01:39:53 +0100

    init comit

---
+++ train_cbm.py
@@ -0,0 +1,444 @@
+"""
+Train InceptionV3 Network using the CUB-200-2011 dataset
+"""
+
+import os
+import sys
+import argparse
+from typing import Dict
+import time
+from datetime import datetime
+
+import yaml
+
+from APN.apn_loss import ProtoModLoss
+from CUB.template_model import End2EndModel
+from utils.train_utils import model_by_mode, normalize_scientific_floats
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import math
+import torch
+import numpy as np
+
+import argparse
+import math
+import os
+import time
+from argparse import Namespace
+from datetime import datetime
+
+import numpy as np
+import torch
+import yaml
+from torch import nn
+
+from cub.config import LR_DECAY_SIZE, MIN_LR
+from cub.dataset import load_data
+from losses import ProtoModLoss
+from utils.train_utils import (
+    compute_accuracies,
+    create_criterions,
+    logger_and_summarywriter,
+    model_by_mode,
+    optimizer_and_scheduler_by_name,
+    normalize_scientific_floats,
+    prepare_model,
+    AverageMeter,
+    LossMeter,
+)
+
+# from CUB import gen_cub_synthetic
+from CUB.dataset import load_data, find_class_imbalance
+from CUB.config import (
+    BASE_DIR,
+    N_CLASSES,
+    N_ATTRIBUTES,
+    UPWEIGHT_RATIO,
+    MIN_LR,
+    LR_DECAY_SIZE,
+)
+
+from torch.utils.tensorboard import SummaryWriter
+from argparse import Namespace
+import torch.nn as nn
+
+def epoch_wrapper(
+    model,
+    optimizer,
+    dataloader,
+    epoch: int,
+    is_training: bool,
+    args: Namespace,
+    tb_writer,
+    cross_entropy: nn.CrossEntropyLoss,
+    protomod_criterion: ProtoModLoss,
+):
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+def run_epoch_simple(
+    model, optimizer, loader, loss_meter, acc_meter, criterion, args, is_training
+):
+    """
+    A -> Y: Predicting class labels using only attributes with MLP
+    """
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    for _, data in enumerate(loader):
+        inputs, labels = data
+        if isinstance(inputs, list):
+            # inputs = [i.long() for i in inputs]
+            inputs = torch.stack(inputs).t().float()
+        inputs = torch.flatten(inputs, start_dim=1).float()
+        inputs_var = inputs.to(device)
+        labels_var = labels.to(device)
+
+        outputs = model(inputs_var)
+        loss = criterion(outputs, labels_var)
+        acc = accuracy(outputs, labels, topk=(1,))
+        loss_meter.update(loss.item(), inputs.size(0))
+        acc_meter.update(acc[0], inputs.size(0))
+
+        if is_training:
+            optimizer.zero_grad()  # zero the parameter gradients
+            loss.backward()
+            optimizer.step()  # optimizer step to update parameters
+    return loss_meter, acc_meter
+
+
+loss_labels = [
+    "total_loss",
+    "concept_loss",
+]
+
+def run_epoch(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    dataloader: torch.utils.data.DataLoader,
+    epoch: int,
+    criterion,
+    attr_criterion,
+    args: Namespace,
+    is_training: bool,
+    tb_writer,
+    cross_entropy: nn.CrossEntropyLoss,
+    protomod_criterion: ProtoModLoss,
+):
+    """
+    For the rest of the networks (X -> A, cotraining, simple finetune)
+    """
+    loss_meter = LossMeter(loss_labels)
+    class_acc_meter = AverageMeter()
+
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    for batch in dataloader:
+        inputs, labels, attr_labels = batch
+
+        # only move to device if needed        
+        if attr_criterion is not None:
+            attr_labels = torch.stack(attr_labels, dim=1).float()
+            attr_labels_var = attr_labels.to(device)
+        
+        
+        inputs, labels = (
+            inputs.to(device),
+            labels.to(device),
+        )
+
+        losses = []
+        
+        if is_training and args.use_aux:
+            outputs, aux_outputs = model(inputs)
+            losses = []
+            out_start = 0
+            
+            if (
+                not args.bottleneck
+            ):  # loss main is for the main task label (always the first output)
+                loss_main = 1.0 * criterion(outputs[0], inputs) + 0.4 * criterion(
+                    aux_outputs[0], inputs
+                )
+                losses.append(loss_main)
+                out_start = 1
+            if (
+                attr_criterion is not None and args.attr_loss_weight > 0
+            ):  # X -> A, cotraining, end2end
+                for i in range(len(attr_criterion)):
+                    losses.append(
+                        args.attr_loss_weight
+                        * (
+                            1.0
+                            * attr_criterion[i](
+                                outputs[i + out_start].squeeze(),
+                                attr_labels_var[:, i],
+                            )
+                            + 0.4
+                            * attr_criterion[i](
+                                aux_outputs[i + out_start].squeeze(),
+                                attr_labels_var[:, i],
+                            )
+                        )
+                    )
+        else:  # testing or no aux logits
+            outputs = model(inputs)
+            losses = []
+            out_start = 0
+            if not args.bottleneck:
+                loss_main = criterion(outputs[0], inputs)
+                losses.append(loss_main)
+                out_start = 1
+            if (
+                attr_criterion is not None and args.attr_loss_weight > 0
+            ):  # X -> A, cotraining, end2end
+                for i in range(len(attr_criterion)):
+                    losses.append(
+                        args.attr_loss_weight
+                        * attr_criterion[i](
+                            outputs[i + out_start].squeeze(),
+                            attr_labels_var[:, i],
+                        )
+                    )
+
+        if args.bottleneck:  # attribute accuracy
+            sigmoid_outputs = torch.nn.Sigmoid()(torch.cat(outputs, dim=1))
+            compute_accuracies(sigmoid_outputs, labels, epoch, class_acc_meter, tb_writer)
+
+        else:
+            acc = accuracy(
+                outputs[0], labels, topk=(1,)
+            )  # only care about class prediction accuracy
+            acc_meter.update(acc[0], inputs.size(0))
+
+        if attr_criterion is not None:
+            if args.bottleneck:
+                total_loss = sum(losses) / args.n_attributes
+            else:  # cotraining, loss by class prediction and loss by attribute prediction have the same weight
+                total_loss = losses[0] + sum(losses[1:])
+                if args.normalize_loss:
+                    total_loss = total_loss / (
+                        1 + args.attr_loss_weight * args.n_attributes
+                    )
+        else:  # finetune
+            total_loss = sum(losses)
+        loss_meter.update(total_loss.item(), inputs.size(0))
+        if is_training:
+            optimizer.zero_grad()
+            total_loss.backward()
+            optimizer.step()
+
+    return loss_meter, acc_meter
+
+
+def train(model: nn.Module, args: Namespace) -> float:
+    """Train function for CUB models.
+
+    Args:
+        model (nn.Module): Model given a specific mode (XY, XCY, etc)
+        args (Namespace): Config arguments
+
+    Returns:
+        float: best validation accuracy
+    """
+    model, _ = prepare_model(model, args)
+    logger, tb_writer = logger_and_summarywriter(args)
+
+    # Determine imbalance
+    imbalance = None
+    if args.use_attr and not args.no_img and args.weighted_loss:
+        train_data_path = os.path.join(BASE_DIR, args.data_dir, "train.pkl")
+        
+        # When arg multiple finds the imbalance ratio for each attr
+        multiple_loss = args.weighted_loss == "multiple"
+        imbalance = find_class_imbalance(train_data_path, multiple_loss)
+
+    logger.write(str(args) + "\n")
+    logger.write("Class Imbalances\n" + str(imbalance) + "\n")
+    logger.flush()
+
+    tb_writer = SummaryWriter(log_dir=os.path.join(args.log_dir, "tensorboard"))
+
+    criterion = torch.nn.CrossEntropyLoss()
+    loss_labels = ["total_loss", "classification_loss", "attribute_reg_loss", "cpt_loss", "decorrelation_loss"]
+    attr_criterion = None
+    if args.use_attr and not args.no_img:
+        attr_criterion = []  # separate criterion (loss function) for each attribute
+        if args.weighted_loss:
+            assert imbalance is not None
+            for ratio in imbalance:
+                attr_criterion.append(
+                    torch.nn.BCEWithLogitsLoss(
+                        weight=torch.FloatTensor([ratio]).to(model.device)
+                    )
+                )
+        else:
+            for i in range(args.n_attributes):
+                attr_criterion.append(torch.nn.CrossEntropyLoss())
+
+    optimizer, scheduler = optimizer_and_scheduler_by_name(model, args)
+
+    best_val_epoch, best_val_acc = -1, 0
+    scheduler_stop_epoch = (
+        int(math.log(MIN_LR / args.lr) / math.log(LR_DECAY_SIZE)) * args.scheduler_step
+    )
+
+    # load data
+    train_loader = load_data(args, "train")
+    val_loader =  load_data(args, "val")
+
+    for epoch in range(0, args.epochs):
+        start_time = time.time()
+
+        # ----- Training -----
+        if args.no_img: # C->Y
+            raise NotImplementedError("TODO")
+            train_loss_meter, train_acc_meter = run_epoch_simple(
+                model,
+                optimizer,
+                train_loader,
+                criterion,
+                args,
+                is_training=True,
+            )
+        else:
+            train_loss_meter, train_acc_meter = run_epoch(
+                model,
+                optimizer,
+                train_loader,
+                criterion,
+                attr_criterion,
+                args,
+                is_training=True,
+            )
+
+
+        # ----- Validation -----
+        with torch.no_grad():
+            if args.no_img:
+                raise NotImplementedError("TODO")
+                val_loss_meter, val_acc_meter = run_epoch_simple(
+                    model,
+                    optimizer,
+                    val_loader,
+                    criterion,
+                    args,
+                    is_training=False,
+                )
+        
+            else:
+                val_loss_meter, val_acc_meter = run_epoch(
+                    model,
+                    optimizer,
+                    val_loader,
+                    criterion,
+                    attr_criterion,
+                    args,
+                    is_training=False,
+                )
+        
+
+        if best_val_acc < val_acc_meter.avg:
+            best_val_epoch = epoch
+            best_val_acc = val_acc_meter.avg
+
+            logger.write("New model best model at epoch %d\n" % epoch)
+            torch.save(
+                model, os.path.join(args.log_dir, "best_model_%d.pth" % args.seed)
+            )
+
+        train_loss_avg = train_loss_meter.avg
+        val_loss_avg = val_loss_meter.avg
+
+        time_duration = time.time() - start_time
+        if model.__class__.__name__ == "ProtoEnd2End":
+
+            logger.write(
+                " - ".join(
+                    [
+                        datetime.now().strftime("%H:%M:%S"),
+                        f"Epoch [{epoch}]",
+                        f"Train/loss: {train_loss_avg:.4f}",
+                        f"Train/acc: {train_acc_meter.avg.item():.4f}",
+                        f"Val/loss: {val_loss_avg:.4f}",
+                        f"Val/acc: {val_acc_meter.avg.item():.4f}"
+                        f"Best val epoch: {best_val_epoch}",
+                        f"Time: {time_duration:.2f} sec",
+                    ]
+                )
+            )
+        else:
+            logger.write(
+                " - ".join([
+                    datetime.now().strftime("%H:%M:%S"),
+                    f"Epoch [{epoch}]",
+                    *[
+                        f"Train/{loss_labels[i]}: {train_loss_meter.avg[i]:.4f}"
+                        for i in range(train_loss_meter.n_losses)
+                    ],
+                    f"Train/acc: {train_acc_meter.avg.item():.4f}",
+                    *[
+                        f"Val/{loss_labels[i]}: {val_loss_meter.avg[i]:.4f}"
+                        for i in range(val_loss_meter.n_losses)
+                    ],
+                    f"Val/acc: {val_acc_meter.avg.item():.4f}",
+                    f"Best val epoch: {best_val_epoch}",
+                    f"Time: {time_duration:.2f} sec",
+                    "\n"
+                ])
+            )
+        logger.flush()
+
+        if epoch <= stop_epoch:
+            scheduler.step(epoch)  # scheduler step to update lr at the end of epoch
+        # inspect lr
+        if epoch % 10 == 0:
+            print("Current lr:", scheduler.get_lr())
+
+        # if epoch % args.save_step == 0:
+        #     torch.save(model, os.path.join(args.log_dir, '%d_model.pth' % epoch))
+
+        if epoch >= 100 and val_acc_meter.avg < 3:
+            print("Early stopping because of low accuracy")
+            break
+        if epoch - best_val_epoch >= 100:
+            print("Early stopping because acc hasn't improved for a long time")
+            break
+
+    # For hyperparameter search
+    return best_val_acc
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        default="configs/debug.yaml",
+        help="Path to config file (YAML)",
+    )
+    cli_args = parser.parse_args()
+
+    # Load the config yaml
+    with open(cli_args.config) as f:
+        args = yaml.safe_load(f)
+    args = normalize_scientific_floats(args)
+
+    # Add run name, keep as namespace to be able to access like args.param
+    args = argparse.Namespace(**args, config_path=cli_args.config)
+
+    model = model_by_mode(args)
+
+    train(model, args)
