579c93e1b6982034442e15d44515b4c96aa918a1
Pascal Makossa <pascal.makossa@stud.tu-darmstadt.de>
2025-12-12 12:01:16 +0100

    Added midterm script for protomod

---
+++ protocbm_midterm.py
@@ -0,0 +1,79 @@
+from argparse import Namespace
+
+from train_protocbm import train
+from utils_protocbm.train_utils import gather_args, model_by_mode
+
+# Model Type, Attribute Regression Loss, Attribute Decorrelation Loss, Map Compactness Loss
+attribute_reg_weight = 1
+attribute_decorrelation_weight = 1
+map_compactness_weight = 1
+
+run_configs = [
+    {
+        "id": "XCY-0",
+        "mode": "XCY",
+        "loss_weight_attribute_reg": 0,
+        "loss_weight_attribute_decorrelation": 0,
+        "loss_weight_map_compactness": 0,
+    },
+    {
+        "id": "XCY-1",
+        "mode": "XCY",
+        "loss_weight_attribute_reg": attribute_reg_weight,
+        "loss_weight_attribute_decorrelation": 0,
+        "loss_weight_map_compactness": 0,
+    },
+    {
+        "id": "XCY-2",
+        "mode": "XCY",
+        "loss_weight_attribute_reg": attribute_reg_weight,
+        "loss_weight_attribute_decorrelation": attribute_decorrelation_weight,
+        "loss_weight_map_compactness": 0,
+    },
+    {
+        "id": "XCY-3",
+        "mode": "XCY",
+        "loss_weight_attribute_reg": attribute_reg_weight,
+        "loss_weight_attribute_decorrelation": attribute_decorrelation_weight,
+        "loss_weight_map_compactness": map_compactness_weight,
+    },
+    {
+        "id": "XC-3",
+        "mode": "XC",
+        "loss_weight_attribute_reg": attribute_reg_weight,
+        "loss_weight_attribute_decorrelation": attribute_decorrelation_weight,
+        "loss_weight_map_compactness": map_compactness_weight,
+    },
+    {
+        "id": "CY",
+        "mode": "CY",
+        "loss_weight_attribute_reg": 0,
+        "loss_weight_attribute_decorrelation": 0,
+        "loss_weight_map_compactness": 0,
+        "checkpoint": "outputs/ProtoCBM-MIDTERM/XC-3/best_model-1.pth",
+    },
+]
+
+
+def main(args: Namespace):
+    for config in run_configs:
+        print(f"Running config: {config['id']}")
+
+        # Deepcopy args to avoid overwriting
+        args = Namespace(**vars(args))
+
+        args.log_dir = f"outputs/midterm/{config['id']}"
+
+        # Copy over config values
+        [setattr(args, key, value) for key, value in config.items() if key != "id"]
+
+        model = model_by_mode(args)
+
+        metric = train(model, args)
+        print(f"Final metric for config {config['id']}: {metric}")
+
+
+if __name__ == "__main__":
+    args = gather_args()
+
+    main(args)
